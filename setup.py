58463f912cf245cf1f880b7e8b9734a63cc44ce0
Anton Trapeznikov <anton.trapeznikov@gmail.com>
2018-01-19 10:44:03 +0500

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+__version__ = '0.0.1'
+
+
+setup(
+    name="ion-mq",
+    version=__version__,
+    author="Anton Trapeznikov",
+    license="http://creativecommons.org/licenses/by-sa/3.0/",
+    url="https://github.com/anton-trapeznikov/ion-mq",
+    platforms=["any"],
+)
